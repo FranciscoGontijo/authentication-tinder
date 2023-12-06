33ac7ae16b3d5368c3cc5c8577439afa412b007c
FranciscoGontijo <franciscoacmg@gmail.com>
2023-12-06 16:31:26 -0300

    Import model and database to index.js

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 
-const express=require('express');
-const mongoose= require('mongoose');
-const bodyparser=require('body-parser');
-const cookieParser=require('cookie-parser');
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const db = require('./src/mongo/connection');
+const userModel = require('./src/models/user');
 
-const app=express();
+const app = express();
+
 // app use
-app.use(bodyparser.urlencoded({extended : false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use(cookieParser());
 
-app.get('/',function(req,res){
+//Requests
+app.get('/', function (req, res) {
     res.status(200).send(`Welcome to login , sign-up api`);
 });
 
+
 // listening port
-const PORT=process.env.PORT||3000;
-app.listen(PORT,()=>{
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log(`app is live at ${PORT}`);
 });
