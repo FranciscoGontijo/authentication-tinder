817272c159eb9bc4fc734b2218037c67d1d095b3
FranciscoGontijo <franciscoacmg@gmail.com>
2024-01-03 09:52:11 -0300

    Changing the whole chat socket, now you only have private rooms to talk to specific person

--- src/api/chatSocket.js
+++ src/api/chatSocket.js
@@ -6,15 +6,23 @@ const init = (server) => {
     io = socketIO(server);
 
     io.on('connection', socket => {
-        console.log('a user connected :D');
-        
-        socket.on('send message', msg => {
-            console.log(msg);
-            io.emit('receive message', msg);
+        console.log('A user connected:', socket.id);
+
+        //Not working need to fix everyone
+        socket.on('login', (userId) => {
+            socket.join(userId); // Join a global room for the user upon login
+            console.log(`User ${userId} joined the global room`);
         });
-    
-        socket.on('user', user => {
-            console.log('message sent by' + user);
+
+        socket.on('openChat', ({ senderId, recipientId }) => {
+            const chatRoomId = [senderId, recipientId].sort().join('-'); // Create a unique room ID for the chat
+            socket.join(chatRoomId); // Join the private room for this chat
+            console.log(`User ${senderId} joined the chat room with ${recipientId}`);
+        });
+
+        socket.on('sendMessage', ({ chatRoomId, message, name, userId }) => {
+            console.log(chatRoomId + ' ' + message);
+            io.to(chatRoomId).emit('Receive Message', {message: message, name: name, userId: userId});
         });
     });
 };
